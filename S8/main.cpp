1be08ab8e57f80830ca53cde8ce75f77783d2b10
Amos Brocco <attila@localhost.localdomain>
2024-05-15 14:19:37 +0200

    15.5.2024

--- S8/main.cpp
+++ S8/main.cpp
@@ -2,6 +2,72 @@
 #include <vector>
 #include "lecturerresearcher.h"
 
+struct Foo {
+    // Soluzione per rendere Foo un tipo polimorfico
+    //virtual ~Foo() = default;
+};
+
+struct Bar : Foo {
+};
+
+
+void dynamiccast() {
+    Employee e{"John", "ISIN", 123};
+    Lecturer l{"Mark", "ISEA", 555, "Computer Science", "C++"};
+    Researcher r{"Alice", "ISEA", 5595, "AI"};
+    LecturerResearcher lr{"John", "ISIN", 3565, "CompSci", "ParallelProgramming", "DS", 50.0};
+
+    //auto el{dynamic_cast<Employee>(l)};
+    Employee el2{l};
+
+    Employee& er{dynamic_cast<Employee&>(r)};
+    Employee& er2{r};
+
+    Employee* ep{dynamic_cast<Employee*>(&r)};
+    Employee* ep2{&r};
+
+    Researcher* rp{dynamic_cast<Researcher*>(ep)}; // virtual base class
+    Researcher* lrp{&lr};
+
+    // Cast valido da un puntatore a Researcher (con "collegato" un LecturerResearcher) a
+    // puntatore a Lecturer
+    Lecturer* lrp_ok{dynamic_cast<Lecturer*>(lrp)};
+    if (lrp_ok == nullptr) {
+        std::cout << "Cast non valido da Researcher* a Lecturer* (con LecturerResearcher)" << std::endl;
+    }
+
+    // Cast NON valido da un puntatore a Researcher (con "collegato" un Researcher) a
+    // puntatore a Lecturer
+    Lecturer* lrp_wrong{dynamic_cast<Lecturer*>(&r)};
+    if (lrp_wrong == nullptr) {
+        std::cout << "Cast non valido da Researcher* a Lecturer* (con Researcher)" << std::endl;
+    }
+
+    // Cast valido da un riferimento a Researcher (con "collegato" un LecturerResearcher) a
+    // riferimento a Lecturer
+    Lecturer& rlrp_ok{dynamic_cast<Lecturer&>(lr)};
+
+    try {
+        // Cast NON valido da un riferimento a Researcher (con "collegato" un LecturerResearcher) a
+        // riferimento a Lecturer
+        Lecturer& rlrp_wrong{dynamic_cast<Lecturer&>(r)};
+    } catch (const std::bad_cast& e) {
+        // cast non valido
+    }
+
+    // Cast NON valido da un puntatore generico (anche se "collegato" un Researcher) a
+    // puntatore a Researcher
+    void* vp{&r};
+    //Researcher* lrrp4{dynamic_cast<Researcher*>(vp)};
+
+
+    Bar b;
+    Foo* f{&b};
+    // Cast NON valido quando la sorgente non è un tipo polimorfico (non ha nessun metodo virtual)
+    auto d{dynamic_cast<Bar*>(f)};
+}
+
+
 void staticcast() {
     Employee e{"John", "ISIN", 123};
     Lecturer l{"Mark", "ISEA", 555, "Computer Science", "C++"};
@@ -19,7 +85,7 @@ void staticcast() {
 
     //Researcher* rp{static_cast<Researcher*>(ep)}; // virtual base class
     Researcher* lrp{&lr};
-    //Lecturer* lrp_wrong{static_cast<Lecturer*>(lrp)};
+    //Lecturer* lrp_ok{static_cast<Lecturer*>(lrp)};
     LecturerResearcher* lr2{static_cast<LecturerResearcher*>(lrp)};
 
     Researcher& lrr{lr};
@@ -29,14 +95,17 @@ void staticcast() {
     Researcher& rr{r};
     LecturerResearcher& lrr3{static_cast<LecturerResearcher&>(rr)};
 
+    Researcher* rrp{&r};
+    LecturerResearcher* lrrp3{static_cast<LecturerResearcher*>(rrp)};
 
-
+    //LecturerResearcher lrrp4{static_cast<LecturerResearcher>(r)};
+    void* vp{&r};
+    LecturerResearcher* lrrp4{static_cast<LecturerResearcher*>(vp)};
 }
 
 
-
 int main() {
-    using namespace std;
+    /*using namespace std;
     LecturerResearcher lr{"John", "ISIN", 3565, "CompSci", "ParallelProgramming", "DS", 50.0};
     cout << "Name: "
          << lr.name()
@@ -72,6 +141,7 @@ int main() {
             std::cout << "Found!\n";
         }
     }
-
-    staticcast();
+    */
+   // staticcast();
+    dynamiccast();
 }
